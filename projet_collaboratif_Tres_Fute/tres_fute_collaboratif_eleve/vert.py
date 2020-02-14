60a211848c55f566a533739c9c2392eb09d2771d
toshi753 <61043571+toshi753@users.noreply.github.com>
2020-02-14 09:25:49 +0100

    Update vert.py

--- projet_collaboratif_Tres_Fute/tres_fute_collaboratif_eleve/vert.py
+++ projet_collaboratif_Tres_Fute/tres_fute_collaboratif_eleve/vert.py
@@ -3,7 +3,7 @@
 ##################
 ## partie verte ##
 ##################
-## Co-auteurs :
+## Co-auteurs : Idir, Lauryne, Robin
 #################
 
 
@@ -11,7 +11,7 @@
 def init_vert():
 	#
 	# à compléter
-	#
+	liste=[False for i in range(1)]
     #return à modifier
 	return None
 
@@ -19,17 +19,31 @@ def init_vert():
 ## affichage de la grille verte :
 def affiche_vert(grille):
 	#
-	# à compléter
+	print(grille)
 	#
 	print()
     
 ## test de positionnement du dé dans la grille verte    
 def test_vert(couleur,des,grille,bonus):
-    #
-	# à compléter
-	#
+    # peut on positionner le dé
+	impossible = True
+	# tuple des contraintes
+	tuple_vert = (1,2,3,4,5,1,2,3,4,5,6)
+	# recherche de la premiere case vide
+	i = 0
+	while i < len(grille) - 1 and grille[i+1]>0:
+		i = i+1
+	if i == 10 and grille[1]>0 : # la grille est pleine
+		print('la grille est pleine')
+	else : # positionnement du dé
+		if tuple_vert[i+1]<=des[couleur]:
+			grille[i+1]=des[couleur]
+			impossible = false
+		else :
+			print('Vous ne pouvez pas placer le dé dans cette grille')
+		
 	#return à modifier
-	return None
+	return impossible,des,grille,bonus
 
 # décompte des points de la grille
 def decompte_vert(grille) :
